fbbdd9b05ac7646f06b6644b20c714f4296b3f12
Jared Necaise <jnecaise@gmail.com>
2024-01-16 09:57:37 -0600

    dap changes

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -10,13 +10,8 @@ vim.keymap.set('n', '<C-k>', '<C-w>k', { silent = true })
 vim.keymap.set('n', '<C-h>', '<C-w>h', { silent = true })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { silent = true })
 
--- Usefull keymap--[[ s ]]
-vim.keymap.set('n', '<C-k>', '<CMD>Man<CR>', { desc = "Open [M]an Page", silent = true })
+-- Useful keymap--[[ s ]]
 vim.keymap.set('n', '<leader>o', 'm`o<ESC>``', { desc = "Insert empty line bellow current line", silent = true })
 vim.keymap.set('n', '<leader>O', 'm`O<ESC>``', { desc = "Insert empty line above current line", silent = true })
 vim.keymap.set('n', '<leader>i', '$i\n<ESC>O',
   { desc = "Move ending character to new line and open line between", silent = true })
-
--- Toggle Neotree
-
--- Telescope Keymaps
--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -8,15 +8,27 @@ return {
 		},
 		config = function()
 			local dap = require("dap")
-			-- local dapui = require("dapui")
+			local dapui = require("dapui")
 			local widgets = require("dap.ui.widgets")
+
+			require("mason-nvim-dap").setup({
+				automatic_setup = true,
+				handlers = {},
+				ensure_installed = {
+					"bash",
+					"codelldb",
+					"python",
+					"delve",
+					"kotlin",
+				},
+			})
 
 			vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
 			vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug: Step Over" })
 			vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Debug: Step Into" })
 			vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Debug: Step Out" })
-			vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
-			vim.keymap.set("n", "<Leader>B", dap.set_breakpoint, { desc = "Debug: Set Breakpoint" })
+			vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+			vim.keymap.set("n", "<Leader>dB", dap.set_breakpoint, { desc = "Debug: Set Breakpoint" })
 			vim.keymap.set("n", "<Leader>lp", function()
 				require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
 			end, { desc = "Debug: Set Log Point" })
@@ -30,6 +42,19 @@ return {
 			vim.keymap.set("n", "<Leader>ds", function()
 				widgets.centered_float(widgets.scopes)
 			end)
+
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
 		end,
 	},
 }
